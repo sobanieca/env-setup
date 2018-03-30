3ec8302d8d869fa4d073b5be38d3680ab9f45e45
Adam Sobaniec <sobanieca@vps433288.ovh.net>
2018-03-30 16:24:15 +0200

    root-ssh-config.sh

--- ssh-config.sh
+++ ssh-config.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-# Run this script as a root user
-
-# Add new user
-adduser sobanieca
-
-echo "run visudo and add following entry below root permissions: $USER ALL=(ALL) NOPASSWD:ALL"
-echo "to change visudo editor type 'sudo update-alternatives --config editor'"
 
 # Create ssh directory in ~ if not exists
 mkdir -p ~/.ssh
@@ -20,5 +13,5 @@ echo "[windows] run puttygen, generate ssh key, save it, copy content to authori
 echo "[windows] if using putty, remember to specify user explicitly (not via prompt)"
 
 echo "[unix] ssh-keygen -t rsa"
-echo "[unix] ssh-copy-id sobanieca@dev.sobaniec.com"
-echo "[unix] alias vps='ssh sobanieca@dev.sobaniec.com -p 22022' in .bashrc"
+echo "[unix] ssh-copy-id user@host"
+echo "[unix] alias vps='ssh user@host -p 22022' in .bashrc"
