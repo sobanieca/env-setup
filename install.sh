04ee0b245319d247141d04c2a50fc00cf977ed45
Adam Sobaniec <sobanieca@vps433288.ovh.net>
2018-11-04 20:26:36 +0100

    install script refactored

--- install.sh
+++ install.sh
@@ -2,13 +2,17 @@
 
 BASE_URL="https://raw.githubusercontent.com/sobanieca/env-setup/master/"
 
+mkdir temp-env-setup
+cd temp-env-setup
+
 wget $BASE_URL"aschema.vim"
 wget $BASE_URL"bashrcx"
 wget $BASE_URL"env-config.sh"
 wget $BASE_URL"tmux.conf"
 wget $BASE_URL"vimrc"
 
-chmod +x env-config.sh
+sudo -E wget -O - $BASE_URL/env-config.sh | bash
 
-. env-config.sh
+cd ..
+rm -rf temp-env-setup
 
