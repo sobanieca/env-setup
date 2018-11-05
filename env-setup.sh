fddb9537004e37fcc4ede140e3d552b078ec95f0
Adam Sobaniec <sobanieca@vps433288.ovh.net>
2018-11-05 21:26:04 +0100

    nvm immediately available script

--- env-setup.sh
+++ env-setup.sh
@@ -37,6 +37,9 @@ echo "Tmux.conf file replaced."
 # Install Node Version Manager
 echo "Installing Node Version Manager with latest Node.js..."
 wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 nvm install node
 nvm use node
 echo "Nvm with latest Node.js installed."
