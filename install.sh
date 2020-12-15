58b79ca228f2c12250a4e4f82c5de68f84d3a3f0
Adam <sobanieca@gmail.com>
2020-12-15 23:19:39 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,10 +5,8 @@ BASE_URL="https://raw.githubusercontent.com/sobanieca/env-setup/master/"
 mkdir env-setup
 cd env-setup
 
-wget $BASE_URL"aschema.vim"
 wget $BASE_URL"bashrcx"
 wget $BASE_URL"tmux.conf"
-wget $BASE_URL"vimrc"
 wget $BASE_URL"env-setup.sh"
 
 cd ..
