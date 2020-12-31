082fc386766085d96e6745b35a21aa2595a2ff94
Adam Sobaniec <sobanieca@gmail.com>
2020-12-31 20:16:46 +0100

    git clone env-setup for termux

--- termux.sh
+++ termux.sh
@@ -14,4 +14,5 @@ git config --global credential.helper store
 mkdir ~/.termux
 wget https://raw.githubusercontent.com/sobanieca/env-setup/master/termux.properties -O ~/.termux/termux.properties
 termux-reload-settings
+git clone https://github.com/sobanieca/env-setup.git
 proot-distro install debian-buster
