4f8734464d1ad54fd51e9c468cedbc2b92f76d03
Adam Sobaniec <sobanieca@gmail.com>
2021-01-01 16:44:33 +0100

    missing file

---
+++ update-configs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+BASE_URL="https://raw.githubusercontent.com/sobanieca/env-setup/master/"
+
+echo "Updating .bashrcx file..."
+wget $BASE_URL"bashrcx" -O $HOME/.bashrcx
+echo ".bashrcx file updated."
+
+echo "Replacing/creating tmux.conf file..."
+wget $BASE_URL"tmux.conf" -O $HOME/.tmux.conf
+echo "Tmux.conf file replaced."
+
+echo "Updating micro editor configs..."
+wget https://raw.githubusercontent.com/sobanieca/env-setup/master/micro/bindings.json -O $HOME/.config/micro/bindings.json
+wget https://raw.githubusercontent.com/sobanieca/env-setup/master/micro/settings.json -O $HOME/.config/micro/settings.json
+echo "Micro editor configs updated"
