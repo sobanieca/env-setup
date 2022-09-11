90fe55164805b33041bde8b500a3ce077fe60143
Adam Sobaniec <sobanieca@gmail.com>
2022-09-11 16:34:03 +0200

    nvm use fix

--- env-setup.sh
+++ env-setup.sh
@@ -70,7 +70,11 @@ echo "Vim installed"
 # Install Node.js
 echo "Installing nvm and node.js..."
 wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 nvm install --lts
+nvm use --lts
 echo "nvm and node.js installed"
 
 # Install Deno
