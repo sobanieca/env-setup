504e021219bf6c0e7b9d2cf67efc4a5423bb19a3
Adam Sobaniec <sobanieca@gmail.com>
2023-10-15 21:05:40 +0000

    termux colors.properties

--- termux.sh
+++ termux.sh
@@ -10,6 +10,7 @@ git config --global user.name "Adam Sobaniec"
 git config --global credential.helper store
 mkdir ~/.termux
 wget https://raw.githubusercontent.com/sobanieca/env-setup/master/termux.properties -O ~/.termux/termux.properties
+wget https://raw.githubusercontent.com/sobanieca/env-setup/master/colors.properties -O ~/.termux/colors.properties
 mkdir nerdfont
 cd nerdfont
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/InconsolataGo.zip -O font.zip
