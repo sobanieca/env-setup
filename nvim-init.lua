f325a5633e6b043ec732bf21e6d6030da524ddfa
Adam <sobanieca@gmail.com>
2023-10-23 23:25:41 +0200

    Update nvim-init.lua

--- nvim-init.lua
+++ nvim-init.lua
@@ -62,7 +62,7 @@ require("lazy").setup({
       require("nvim-tree").setup {
         sort_by = "case_sensitive",
         view = {
-          width = 30,
+          width = 60,
         },
         renderer = {
           group_empty = true,
