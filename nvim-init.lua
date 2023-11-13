ed0e2e65447ca536c01b6aed9fe2ed8c800e1e25
Adam Sobaniec <sobanieca@gmail.com>
2023-11-13 21:11:15 +0000

    treesitter enabled

--- nvim-init.lua
+++ nvim-init.lua
@@ -163,7 +163,6 @@ require("lazy").setup({
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     event = "VeryLazy",
-enabled = false,
     config = function()
       local configs = require("nvim-treesitter.configs")
 
