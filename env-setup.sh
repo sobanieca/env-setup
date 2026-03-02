5e7ac45fb61f8cd9764021d6b92deba35f508e4b
Adam Sobaniec <sobanieca@gmail.com>
2026-03-02 21:59:19 +0100

    chore: install github cli and docker

--- env-setup.sh
+++ env-setup.sh
@@ -116,8 +116,43 @@ echo "Installing lsd..."
 sudo apt-get install lsd -y
 echo "Lsd installed."
 
+# Install Github CLI
+(type -p wget >/dev/null || (sudo apt update && sudo apt install wget -y)) \
+	&& sudo mkdir -p -m 755 /etc/apt/keyrings \
+	&& out=$(mktemp) && wget -nv -O$out https://cli.github.com/packages/githubcli-archive-keyring.gpg \
+	&& cat $out | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
+	&& sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
+	&& sudo mkdir -p -m 755 /etc/apt/sources.list.d \
+	&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+	&& sudo apt update \
+	&& sudo apt install gh -y
+
+# Install Docker
+sudo apt update
+sudo apt install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/debian
+Suites: $(. /etc/os-release && echo "$VERSION_CODENAME")
+Components: stable
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+
+sudo apt update
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo groupadd docker
+sudo usermod -aG docker $USER
+
 # Update-configs script
 wget $BASE_URL"update-configs" -O $HOME/tools/update-configs
 chmod +x $HOME/tools/update-configs
 . $HOME/tools/update-configs
 
+echo "Environment configured. Now do the following:"
+echo "* Open tmux and press ctrl+b,I to install tmux plugins"
+echo "* Run `gh auth login` to authenticate in Github CLI"
+echo "* Log out and log in so membership is re-evaluated to access Docker without sudo"
